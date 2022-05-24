6617caa899f8b2c4b8109b35f71a56bda2c04761
David <davidrlutrick@gmail.com>
2022-05-23 21:28:34 -0700

    Added randomNumber from 1-100

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class NumberGuessingGame {
     public static void main(String [] args) {
-        
+        int randomNumber = (int) (Math.random() * 100) + 1;
     }
 }
