19141c77abe74f745ffce4b2c9c980907284e8db
David <davidrlutrick@gmail.com>
2022-05-23 21:49:34 -0700

    added game loop

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,6 +1,27 @@
 import java.util.Scanner;
 public class NumberGuessingGame {
     public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
         int randomNumber = (int) (Math.random() * 100) + 1;
+        boolean win = false;
+        System.out.println("Please guess a number between 1 and 100.");
+        for(int i = 9; i > 0; i--) {
+            int guess = scanner.nextInt();
+            if(guess > randomNumber) {
+                System.out.println("Your guess is too high.");
+                System.out.println("You have " + i + " guess(es) left.");
+            } else if(guess < randomNumber) {
+                System.out.println("Your guess is too low.");
+                System.out.println("You have " + i + " guess(es) left.");
+            } else if(guess == randomNumber) {
+                System.out.println("You guessed right!");
+                System.out.println("You have " + i + " guess(es) left.");
+                win = true;
+                break;
+            }
+        }
+        if(win == true) {
+            System.out.println("You win!");
+        }
     }
 }
